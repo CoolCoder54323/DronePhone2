042adcc3844904fbaa3496c17717158d6c0f15ca
Chef Pops <ddixonjr@gmail.com>
2021-01-16 15:30:43 -0500

    Initial Commit

---
+++ DronePhone/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DronePhone
+//
+//  Created by DENNIS DIXON on 1/16/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
